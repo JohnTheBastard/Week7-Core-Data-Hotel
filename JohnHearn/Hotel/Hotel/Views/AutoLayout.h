3020334144c93771a70cc7da71608ac5efcf8ba8
John Hearn <jhearn@gmail.com>
2016-11-28 10:44:27 -0800

    created AutoLayout class and constraint methods

---
+++ JohnHearn/Hotel/Hotel/Views/AutoLayout.h
@@ -0,0 +1,43 @@
+//
+//  AutoLayout.h
+//  Hotel
+//
+//  Created by John D Hearn on 11/28/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AutoLayout : NSObject
+
++(NSLayoutConstraint *)createGenericConstraintFrom:(UIView *)view
+                                            toView:(UIView *)superView
+                                     withAttribute:(NSLayoutAttribute)attribute
+                                     andMultiplier:(CGFloat)multiplier;
+
++(NSLayoutConstraint *)createGenericConstraintFrom:(UIView *)view
+                                            toView:(UIView *)superView
+                                     withAttribute:(NSLayoutAttribute)attribute;
+
++(NSArray *)activateFullViewConstraintsUsingVFLFor:(UIView *)view;
+
+// Just to show the difference from VFL
++(NSArray *)activateFullViewConstraintsFrom:(UIView *)view
+                                     toView:(UIView *)superView;
+
++(NSLayoutConstraint *)createLeadingConstraintFrom:(UIView *)view
+                                            toView:(UIView *)superView;
+
++(NSLayoutConstraint *)createTrailingConstraintFrom:(UIView *)view
+                                             toView:(UIView *)superView;
+
++(NSLayoutConstraint *)createEqualHeightConstraintFrom:(UIView *)view
+                                                toView:(UIView *)otherView;
+
++(NSLayoutConstraint *)createLeadingConstraintFrom:(UIView *)view
+                                            toView:(UIView *)otherView
+                                    withMultiplier:(CGFloat)multiplier;
+
+
+
+@end
