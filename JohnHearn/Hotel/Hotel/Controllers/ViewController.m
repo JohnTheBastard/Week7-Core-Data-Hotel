088e4f6836181e3c47e3347afa11116f70b52709
John Hearn <jhearn@gmail.com>
2016-11-28 09:29:29 -0800

    project setup

---
+++ JohnHearn/Hotel/Hotel/Controllers/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Hotel
+//
+//  Created by John D Hearn on 11/28/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+-(void)loadView{
+    [super loadView];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
