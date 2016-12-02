9e9167f9bb9671f654bc903e229c32d4063ff1d6
John Hearn <jhearn@gmail.com>
2016-12-01 17:21:51 -0800

    added ReservationTableViewCell (but haven't implemented any functionality)

---
+++ JohnHearn/Hotel/Hotel/Views/ReservationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ReservationTableViewCell.h
+//  Hotel
+//
+//  Created by John D Hearn on 12/1/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReservationTableViewCell : UITableViewCell
+
+@end
---
+++ JohnHearn/Hotel/Hotel/Views/ReservationTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ReservationTableViewCell.m
+//  Hotel
+//
+//  Created by John D Hearn on 12/1/16.
+//  Copyright © 2016 Bastardized Productions. All rights reserved.
+//
+
+#import "ReservationTableViewCell.h"
+
+@implementation ReservationTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
